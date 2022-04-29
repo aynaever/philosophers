2947800843d8916fa36c68f7bb8032673a9f6110
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-04-29 15:09:00 +0000

    fix norm errors in philo.h

--- philo/philo.h
+++ philo/philo.h
@@ -1,10 +1,14 @@
-/**
- * File              : philo.h
- * Author            : Aymane N <anaouadi@students.42wolfsburg.de>
- * Date              : 17.04.2022
- * Last Modified Date: 17.04.2022
- * Last Modified By  : Aymane N <anaouadi@students.42wolfsburg.de>
- */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaouadi <anaouadi@student.42wolfsbu       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 14:53:56 by anaouadi          #+#    #+#             */
+/*   Updated: 2022/04/29 15:08:33 by anaouadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
@@ -15,38 +19,36 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-# define	LEFT		(philo->i + philo->infos->nPhilos - 1) % philo->infos->nPhilos
-# define	RIGHT		(philo->i + 1) % philo->infos->nPhilos
-# define	RED			"\033[31m"
-# define	CYAN		"\033[36m"
-# define	YELLOW		"\033[33m"
-# define	BLUE		"\033[34m"
-# define	GREEN		"\033[32m"
+# define RED "\033[31m"
+# define CYAN "\033[36m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define GREEN "\033[32m"
 
-typedef	struct	info_s
+typedef struct s_info
 {
-	int					nPhilos;
-	int					timeToDie;
-	int					timeToEat;
-	int					timeToSleep;
-	int*				forks;
-	pthread_mutex_t**	lock;
-	pthread_t**			threads;
-}	info_t;
+	int					n_philos;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					*forks;
+	pthread_mutex_t		**lock;
+	pthread_t			**threads;
+}	t_info;
 
-typedef	struct	philo_s
+typedef struct s_philo
 {
-	int					lastMeal;
+	int					last_meal;
 	int					i;
-	info_t*				infos;
-}	philo_t;
+	t_info				*infos;
+}	t_philo;
 
-void*		p_eat ( void* value );
-void*		p_think ( void* value );
-void*		p_sleep ( void* value );
-void*		p_dine ( void* value );
-suseconds_t	printTimeStamp ( void );
-int			init_args ( info_t* info, int argc, char** argv );
-int     	ft_atoi(const char *nptr);
+void		*p_eat(void *value);
+void		*p_think(void *value);
+void		*p_sleep(void *value);
+void		*p_dine(void *value);
+suseconds_t	printTimeStamp(void);
+int			init_args(info_t *info, int argc, char **argv);
+int			ft_atoi(const char *nptr);
 
 #endif /* ifndef PHILO_H */
